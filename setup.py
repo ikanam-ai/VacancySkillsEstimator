08a2895e3f013bb5732a523a5d19cce28ad379dc
Chetand1228 <74776380+Chetoff1228@users.noreply.github.com>
2024-04-27 03:06:27 +0300

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='VacancySkillsEstimator - проект, который анализирует вакансии на hh.ru и связывает их с соответствующими образовательными курсами. Позволяет соискателям понять, какие навыки нужны работодателям, и предоставляет информацию о доступных курсах для их приобретения.',
+    author='ikanam-ai',
+    license='MIT',
+)
